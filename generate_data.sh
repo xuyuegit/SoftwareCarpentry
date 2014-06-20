cd8fd45f830693d56d1058c088b74db467c0f915
Susanna Strada <ss2733@Susanna-Stradas-MacBook-Pro.local>
2014-06-20 11:17:10 -0400

    This is my first commit, and I've added generate_data.sf, which works.

---
+++ generate_data.sh
@@ -0,0 +1,14 @@
+# Purpose : use a do loop to create a text file called data1.txt with lines 1->10
+
+#$FILENAME="data1.txt"
+$COUNTER=0
+
+#touch $FILENAME
+touch data1.txt
+
+for i in {1..10};
+do
+  echo The counter is at $COUNTER
+  let COUNTER=COUNTER+1
+  echo $COUNTER>>data1.txt
+done
