d3b64a59a45c2db8d891f70869f8d886011fb51b
sumisura <ss2733@Susanna-Stradas-MacBook-Pro.local>
2014-06-20 13:55:40 -0400

    Made 2 columns in generate_data

--- generate_data.sh
+++ generate_data.sh
@@ -1,13 +1,9 @@
-# Purpose : use a do loop to create a text file called data1.txt with lines 1->10
-
-#$FILENAME="data1.txt"
-$COUNTER=0
+# Purpose : use a do loop to create a text file called data1.txt with lines 1->10 and 2 columns
 
 touch data1.txt
 
 for i in {1..10};
 do
-  echo The counter is at $COUNTER
-  let COUNTER=COUNTER+1
-  echo $COUNTER>>data1.txt
+  let j=i+1
+  echo $j $i >>data1.txt
 done
